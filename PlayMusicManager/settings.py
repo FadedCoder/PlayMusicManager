ebdf512d641459ace606d89f40918bcb0f102286
Soham Sen <sohamsen1000@gmail.com>
2018-05-15 21:54:14 +0530

    Fucking typo

--- PlayMusicManager/settings.py
+++ PlayMusicManager/settings.py
@@ -135,7 +135,7 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.joib(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
